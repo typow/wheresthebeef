8bd14ba2025dc24ca7e4a8646baf3b63357d4cde
jacobhalmus@gmail.com <jacobhalmus@gmail.com@0320c796-22e4-f666-dd2e-cc4b1ab4d4bf>
2013-10-14 15:38:24 +0000

    Initial commit to verify working SVN

---
+++ src/View/CMS_Main.java
@@ -0,0 +1,10 @@
+package View;
+
+import java.io.IOException;
+
+public class CMS_Main {
+	public static void main(final String[] the_args) throws IOException
+	{
+		System.out.println("Where's the beef?");
+	}
+}
