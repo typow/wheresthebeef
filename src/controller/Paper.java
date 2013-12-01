b6bc468e5a920776ab5863dfb98510167c51db3e
jacobhalmus@gmail.com <jacobhalmus@gmail.com@0320c796-22e4-f666-dd2e-cc4b1ab4d4bf>
2013-12-01 18:02:51 +0000

    finished paper object, still need to comment

--- src/controller/Paper.java
+++ src/controller/Paper.java
@@ -11,7 +11,7 @@ public class Paper {
 	private paperStatusAdminViewable status_admin_viewable;
 	private String sub_pc = "";
 	private String pc = "";
-	private String[] reviewers = new String[4];
+	private String[] reviewers;
 	private int id;
 	
 	public Paper(final String the_conf_title, final String the_paper_title, 
@@ -20,5 +20,46 @@ public class Paper {
 			final paperStatusAdminViewable the_status_admin_viewable,
 			final String the_sub_pc, final String the_pc, final String[] the_reviewers,
 			final int the_id){	
+		conf_title = the_conf_title;
+		paper_title = the_paper_title;
+		author = the_author;
+		status_author_viewable = the_status_author_viewable;
+		status_admin_viewable = the_status_admin_viewable;
+		sub_pc = the_sub_pc;
+		pc = the_pc;
+		reviewers = the_reviewers;
+		id = the_id;
+	}
+	
+	public String getConfTitle(){
+		return conf_title;
+	}
+	
+	public String getPaperTitle(){
+		return paper_title;
+	}
+	
+	public String getAuthor(){
+		return author;
+	}
+	
+	public paperStatusAuthorViewable getStatusAuthorViewable(){
+		return status_author_viewable;
+	}
+	
+	public paperStatusAdminViewable getStatusAdminViewable(){
+		return status_admin_viewable;
+	}
+	
+	public String getSubPC(){
+		return pc;
+	}
+	
+	public String[] getReviewers(){
+		return reviewers;
+	}
+	
+	public int getID(){
+		return id;
 	}
 }
