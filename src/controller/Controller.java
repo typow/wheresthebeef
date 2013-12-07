cfa7ac7071d0da317f79cd0b07704dacb271a4df
sethk2@uw.edu <sethk2@uw.edu@0320c796-22e4-f666-dd2e-cc4b1ab4d4bf>
2013-12-07 06:48:49 +0000

    slight fix to controller

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -17,7 +17,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
